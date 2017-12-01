1eb777db5c658a565361b84ab717ac82f918bed7
Rage343 <wh48082@yahoo.com>
2017-12-01 14:59:22 -0400

    first commit

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -21,30 +21,30 @@ class Nav extends Component {
     }
 
   render() {
+
       const classActive = (this.state.isActive ? 'is-active' : '');
 
     return (
         <nav className="navbar is-transparent navbar-main">
-        <div className="container">
-            <div className="navbar-brand">
-                <a className="navbar-item" href="https://hernandez2.com">
-                <img src="" alt="Logo" width="112" height="28"/>
-                </a>
-                <div className={"navbar-burger burger "  + classActive } onClick={this.menuToggle} data-target="navbar-menu">
-                <span></span>
-                <span></span>
-                <span></span>
+            <div className="container">
+                <div className="navbar-brand">
+                    <a className="navbar-item" href="https://hernandez2.com">
+                    <img src="" alt="Logo" width="112" height="28"/>
+                    </a>
+                    <div className={"navbar-burger burger "  + classActive } onClick={this.menuToggle} data-target="navbar-menu">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    </div>
+                </div>
+                <div id="navbar-menu" className={"navbar-menu " + classActive }>
+                <div className="navbar-end">
+                    <a className="navbar-item" href="/about"> About </a>
+                    <a className="navbar-item" href="/blog"> Blog </a>
+                </div>
                 </div>
             </div>
-            <div id="navbar-menu" className={"navbar-menu " + classActive }>
-            <div className="navbar-end">
-                <a className="navbar-item" href="/about"> About </a>
-                <a className="navbar-item" href="/blog"> Blog </a>
-            </div>
-            </div>
-        </div>
-        
-      </nav>
+        </nav>
     );
   }
 }
