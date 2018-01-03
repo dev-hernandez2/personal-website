5eb0f98a07c302ac3b91155f906ca82c9740f997
Rage343 <wh48082@yahoo.com>
2018-01-03 13:30:43 -0400

    Refractory and and footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,35 @@
 import React from "react";
 
+import Row from "../Grids/Row";
+import Column from "../Grids/Column";
+
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
         <div className="content has-text-centered">
-          <p />
+          <Row>
+            <Column>
+              <a href="/#" target="_black">
+                Github
+              </a>
+            </Column>
+            <Column>
+              <a href="/#" target="_black">
+                Linkedin
+              </a>
+            </Column>
+            <Column>
+              <a href="/#" target="_black">
+                Twitter
+              </a>
+            </Column>
+            <Column>
+              <a href="/#" target="_black">
+                Download CV
+              </a>
+            </Column>
+          </Row>
         </div>
       </div>
     </footer>
--- src/components/Form/Field/index.js
+++ src/components/Form/Field/index.js
@@ -1,23 +1,32 @@
-import React from 'react'
-
+import React from "react";
 
 const Field = props => (
-            <div className="field">
-                <label className="label">{props.label}</label>
-                <div className="control">
-                     {(()=>{
-                        if(props.inputDisplay === 'textarea') {
-                            return (
-                                <textarea className="textarea" id={props.inputId}  placeholder={props.placeholder} rows="10"></textarea>
-                            )
-                        } else {
-                            return (
-                                <input className="input" id={props.inputId} type={props.inputType} placeholder={props.placeholder}/>
-                            )
-                        }
-                    })()}
-                </div>
-            </div>
-)
+  <div className="field">
+    <label className="label">{props.label}</label>
+    <div className="control">
+      {(() => {
+        if (props.inputDisplay === "textarea") {
+          return (
+            <textarea
+              className="textarea"
+              id={props.inputId}
+              placeholder={props.placeholder}
+              rows="10"
+            />
+          );
+        } else {
+          return (
+            <input
+              className="input"
+              id={props.inputId}
+              type={props.inputType}
+              placeholder={props.placeholder}
+            />
+          );
+        }
+      })()}
+    </div>
+  </div>
+);
 
-export default Field
+export default Field;
