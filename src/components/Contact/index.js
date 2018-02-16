6f577bf9b84e49a35d87d8c5873725c3c21f7ffc
Rage343 <wh48082@yahoo.com>
2018-02-16 02:12:40 -0400

    working on form client side valitadion

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -14,39 +14,102 @@ class Contact extends Component {
     name: '',
     massage: '',
     response: '',
-    isSend: false
+    isMailSent: false,
+    errors: {
+      email: false,
+      name: false,
+      massage: false
+    },
+    isDisabled: false
   }
+
+  isValidEmail = email => {
+    if (/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email)) { return true }
+  }
+
   cleanForm = () => { 
     document.getElementById("Contactform").reset();
   }
+
+  handleEmailChange = email => {
+    if(this.isValidEmail(email.target.value)){
+
+      this.setState({ 
+        email: email.target.value,
+        errors: {
+          email: false,
+        },
+        isDisabled: false
+     });
+      console.log(this.state.errors);
+    } else {
+
+      this.setState({ 
+        errors: {
+          email: true,
+        },
+        isDisabled: true
+     });
+     this.setState({ 
+      email: email.target.value,
+   });
+    console.log(this.state.email);
+     console.log(this.state.errors);
+    }
+  }
+
   handleInputChange = event => {
+
     const target = event.target;
     const value = target.value;
     const name = target.name;
+    if(value) {
+      this.setState({
+        [name]: value,
+          errors: {
+            [name]: false
+          },
+          isDisabled: false
+      });
+    }else {
+        this.setState({ 
+          errors: {
+            [name]: true
+          },
+          isDisabled: true
+      });
+    }
 
-    this.setState({
-      [name]: value
-    });
   }
 
   handleSubmit = event => {
     event.preventDefault();
-    var params = new URLSearchParams();
-    params.append('name', this.state.name);
-    
-    axios.post('http://localhost/contact-post/', params)
-    .then(response => {
-      this.setState({response: response.data, isSend: true });
-      console.log(response.data);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-    this.cleanForm();
-    console.log(this.state.name, this.state.email, this.state.massage);
+
+    const { name, email, massage,  isDisabled } = this.state;
+
+    const params = new URLSearchParams();
+    params.append('name', name);
+
+    if(isDisabled) {
+      // axios.post('http://localhost/contact-post/', params)
+      // .then(response => {
+      //   this.setState({response: response.data,  isMailSent: true });
+      //   console.log(response.data);
+      // })
+      // .catch(error => {
+      //   console.log(error);
+      // });
+      // this.cleanForm();
+      console.log(name, email, massage);
+    } 
+   
   }
   render() {
-    const classActive = this.state.isSend ? "" : "is-invisible";
+    const classActive = this.state.isMailSent ? "" : "is-invisible";
+    const emailError = this.state.errors.email ? "" : "is-invisible";
+    const nameError = this.state.errors.name ? "" : "is-invisible";
+    const massageError = this.state.errors.massage ? "" : "is-invisible";
+    const formError = this.state.isDisabled ? "" : "is-invisible";
     return (
       <Section
         backgroundColor="bg-section-color-grey"
@@ -76,6 +139,7 @@ class Contact extends Component {
           <div className="box box-contact">
             
             <form onSubmit={this.handleSubmit.bind(this)} id="Contactform">
+            <p className={"help is-danger " + formError}>Every imput is required</p>
             <div className="field">
               <label className="label">Name:</label>
               <div className="control">
@@ -88,6 +152,8 @@ class Contact extends Component {
                 onChange={this.handleInputChange.bind(this)}
                 />
               </div>
+              
+              <p className={"help is-danger " + nameError}>This Name is required</p>
             </div>
             <div className="field">
               <label className="label">Email:</label>
@@ -95,12 +161,14 @@ class Contact extends Component {
                 <input
                   className="input"
                   id="email"
-                  type="text"
+                  type="email"
                   placeholder="your email"
                   name="email"
-                  onChange={this.handleInputChange.bind(this)}
+                  onChange={this.handleEmailChange.bind(this)}
+
                 />
               </div>
+              <p className={"help is-danger " + emailError}>This email is invalid</p>
             </div>
             <div className="field">
             <label className="label">Massage:</label>
@@ -114,6 +182,7 @@ class Contact extends Component {
                   onChange={this.handleInputChange.bind(this)}
                 />
               </div>
+              <p className={"help is-danger " + massageError}>This Massage is required</p>
             </div>
          
               <h1 className={"has-text-primary is-size-4 has-text-centered " + classActive}>{this.state.response}</h1>
